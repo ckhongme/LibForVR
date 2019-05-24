ea9e49e0b18703f8ac723610285948107ff68a36
ckhongme <ckhonglogin@163.com>
2019-05-24 20:24:41 +0800

    init vr project

---
+++ AboutSteamVR/SteamVR2.0+/Assets/Scripts/ForViveInputUtility.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using HTC.UnityPlugin.Vive;
+using HTC.UnityPlugin.VRModuleManagement;
+
+public class ForViveInputUtility : MonoBehaviour
+{
+	void Start ()
+    {
+        
+        var index = VRModule.GetRightControllerDeviceIndex();
+        if (!VRModule.IsValidDeviceIndex(index)) return;
+
+        var index1 = ViveRole.GetDeviceIndex(HandRole.RightHand);
+        if (!ViveRole.IsValidIndex(0)) return;
+
+        ViveInput.GetPressDown(HandRole.RightHand, ControllerButton.Trigger);
+
+    }
+	
+	
+	void Update () {
+		
+	}
+}
